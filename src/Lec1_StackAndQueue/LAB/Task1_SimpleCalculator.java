85aa76da5739d72d99eba7206eed3bb23a862165
Desislav Gradev <desislav.gradev@gmail.com>
2022-05-20 08:33:14 +0300

    Some Tasks

---
+++ src/Lec1_StackAndQueue/LAB/Task1_SimpleCalculator.java
@@ -0,0 +1,28 @@
+package Lec1_StackAndQueue.LAB;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Task1_SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arr = scanner.nextLine().split("\\s+");
+        ArrayDeque<String> buffer = new ArrayDeque<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            buffer.push(arr[i]);
+        }
+        int sum = Integer.parseInt(buffer.pop());
+
+        while (!buffer.isEmpty()) {
+            String operator = buffer.pop();
+            int operand = Integer.parseInt(buffer.pop());
+            if (operator.equals("+")) {
+                sum += operand;
+            } else {
+                sum -= operand;
+            }
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/Lec1_StackAndQueue/LAB/Task5_PrinterQueue.java
@@ -0,0 +1,16 @@
+package Lec1_StackAndQueue.LAB;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Task5_PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        ArrayDeque<String> printer = new ArrayDeque<>();
+
+        while (!input.equals("print")) {
+            
+        }
+    }
+}
