875c8d77cc8bc3af9602e45f5c9f384506ce9b6f
Desislav Gradev <desislav.gradev@gmail.com>
2022-05-30 23:49:27 +0300

    Sets and Maps

---
+++ src/Lec2_MultidimetionalArrays/Exercises/Task6_StringMatrixRotation.java
@@ -0,0 +1,10 @@
+package Lec2_MultidimetionalArrays.Exercises;
+
+import java.util.Scanner;
+
+public class Task6_StringMatrixRotation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+                
+    }
+}
---
+++ src/Lec3_SetsAndMapsAdvanced/LAB/Task4_CountRealNumbers.java
@@ -0,0 +1,27 @@
+package Lec3_SetsAndMapsAdvanced.LAB;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Task4_CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] input = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+        Map<Double, Integer> numbersAndOccurrences = new LinkedHashMap<>();
+        for (double value : input) {
+            Integer currentValue = numbersAndOccurrences.get(value);
+            if (currentValue == null) {
+                numbersAndOccurrences.put(value, 1);
+            } else {
+                numbersAndOccurrences.put(value, currentValue +1);
+            }
+        }
+        for (var entry : numbersAndOccurrences.entrySet()) {
+            System.out.printf("%.1f -> %d%n", entry.getKey(), entry.getValue());
+        }
+    }
+}
