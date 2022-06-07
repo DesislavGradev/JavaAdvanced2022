d38adf1da18297abdf7ff3cf995edc78b1e04f49
Desislav Gradev <desislav.gradev@gmail.com>
2022-06-07 23:54:20 +0300

    Minor changes

--- src/Lec6_DefiningClasses/Exercises/CompanyRoster/Employee.java
+++ src/Lec6_DefiningClasses/Exercises/CompanyRoster/Employee.java
@@ -2,7 +2,8 @@ package Lec6_DefiningClasses.Exercises.CompanyRoster;
 
 public class Employee {
 
-    private String name;;
+    private String name;
+    ;
     private double salary;
     private String position;
     private String department;
@@ -11,9 +12,11 @@ public class Employee {
 
     public Employee(String name, double salary, String position, String department) {
         this.name = name;
-        this. salary = salary;
-        this. position = position;
+        this.salary = salary;
+        this.position = position;
         this.department = department;
+        this.email = "n/a";
+        this.age = -1;
     }
 
     public Employee(String name, double salary, String position, String department, String email) {
@@ -30,5 +33,6 @@ public class Employee {
         this(name, salary, position, department, email);
         this.age = age;
     }
+
 
 }
