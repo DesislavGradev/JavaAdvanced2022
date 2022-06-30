d1ed2f8da61c28156f8733fb9879051ad056d107
Desislav Gradev <desislav.gradev@gmail.com>
2022-06-30 11:32:20 +0300

    oop - working with abstraction

---
+++ src/OOP/Lec1_WorkingWithAbstraction/Task2_PointOfRectangle/Main.java
@@ -0,0 +1,39 @@
+package OOP.Lec1_WorkingWithAbstraction.Task2_PointOfRectangle;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] rectCoordinates = getPointCoordinates(scanner);
+
+        int bottomLeftX = rectCoordinates[0];
+        int bottomLeftY = rectCoordinates[1];
+        int topRightX = rectCoordinates[2];
+        int topRightY = rectCoordinates[3];
+
+        Point pointA = new Point(bottomLeftX, bottomLeftY);
+        Point pointC = new Point(topRightX, topRightY);
+
+        Rectangle rectangle = new Rectangle(pointA, pointC);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        while (n-- > 0){
+            int[] pointCoordinates = getPointCoordinates(scanner);
+            int pointX = pointCoordinates[0];
+            int pointY = pointCoordinates[1];
+            Point point = new Point(pointX, pointY);
+
+            System.out.println(rectangle.contains(point));
+        }
+    }
+
+    private static int[] getPointCoordinates(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+}
---
+++ src/OOP/Lec1_WorkingWithAbstraction/Task2_PointOfRectangle/Rectangle.java
@@ -0,0 +1,16 @@
+package OOP.Lec1_WorkingWithAbstraction.Task2_PointOfRectangle;
+
+public class Rectangle {
+    private Point a;
+    private Point c;
+
+    public Rectangle(Point a, Point c) {
+        this.a = a;
+        this.c = c;
+    }
+
+
+    public boolean contains(Point point) {
+        return point.greaterOrEqual(a) && point.lesserOrEqual(c);
+    }
+}
