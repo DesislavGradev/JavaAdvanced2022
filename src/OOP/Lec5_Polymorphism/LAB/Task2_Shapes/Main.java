61d7435ea53fe08edb3819c104d824d18dd8a737
Desislav Gradev <desislav.gradev@gmail.com>
2022-07-14 07:07:24 +0300

    Polymorphism LAB

---
+++ src/OOP/Lec5_Polymorphism/LAB/Task2_Shapes/Main.java
@@ -0,0 +1,8 @@
+package OOP.Lec5_Polymorphism.LAB.Task2_Shapes;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.5);
+        System.out.println(circle.calculateArea());
+    }
+}
